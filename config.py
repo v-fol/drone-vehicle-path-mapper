23b9a246e95442f834b29454fc0a142c332eb4fb
Volodymyr Foliush <flenderinfo@gmail.com>
2024-12-16 09:37:58 +0200

    [feature] formated:ruff

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 class Config:
-    YOLO_MODEL_PATH = './runs/detect/train_small_v11_35epochs/weights/best.pt'
-    DRONE_DATA_PATH = 'parsedSRT.json'
-    VIDEO_PATH = 'video_low_bit.mp4'
-    GEOJSON_OUTPUT_PATH = './demo/frontend/src/pathGEO.json'
-    CAR_IMAGE_PATH = './demo/frontend/src/assets/images/vehicles'
+    YOLO_MODEL_PATH = "./runs/detect/train_small_v11_35epochs/weights/best.pt"
+    DRONE_DATA_PATH = "parsedSRT.json"
+    VIDEO_PATH = "video_low_bit.mp4"
+    GEOJSON_OUTPUT_PATH = "./demo/frontend/src/pathGEO.json"
+    CAR_IMAGE_PATH = "./demo/frontend/src/assets/images/vehicles"
 
-    CAMERA_RESOLUTION_WIDTH = 1920 # pixels
-    CAMERA_RESOLUTION_HEIGHT = 1080 # pixels
-    SENSOR_WIDTH = 8 # mm
-    
+    CAMERA_RESOLUTION_WIDTH = 1920  # pixels
+    CAMERA_RESOLUTION_HEIGHT = 1080  # pixels
+    SENSOR_WIDTH = 8  # mm
+
     CONFIDENCE_THRESHOLD = 0.8
     IOU_THRESHOLD = 0.9
     CLOSE_TO_FRAME_PIXELS = 20
